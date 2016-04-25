b902dd8344628dfba952b60c17bac89f120cc6ff
Denis Tarasov <dvtarasov@edu.hse.ru>
2016-04-25 21:30:52 +0300

    Not working update
    I really tried but it doesn't work

---
+++ app/src/main/java/com/example/mobilization_music_app/JSONParserActivity.java
@@ -0,0 +1,101 @@
+package com.example.mobilization_music_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class JSONParserActivity extends AppCompatActivity {
+
+    public static String LOG_TAG = "my_log";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_parser);
+        new ParseTask().execute();
+    }
+
+    private class ParseTask extends AsyncTask<Void, Void, String> {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        // Copypasted from Internet
+        // Receiving data
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                URL url = new URL("http://download.cdn.yandex.net/mobilization-2016/artists.json");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // Output resulting JSON String for debugging
+            Log.d(LOG_TAG, strJson);
+
+            JSONObject dataJsonObj = null;
+            String secondName = "";
+
+            try {
+                dataJsonObj = new JSONObject(strJson);
+                JSONArray friends = dataJsonObj.getJSONArray("friends");
+
+                // 1. достаем инфо о втором друге - индекс 1
+                JSONObject secondFriend = friends.getJSONObject(1);
+                secondName = secondFriend.getString("name");
+                Log.d(LOG_TAG, "Второе имя: " + secondName);
+
+                // 2. перебираем и выводим контакты каждого друга
+                for (int i = 0; i < friends.length(); i++) {
+                    JSONObject friend = friends.getJSONObject(i);
+
+                    JSONObject contacts = friend.getJSONObject("contacts");
+
+                    String phone = contacts.getString("mobile");
+                    String email = contacts.getString("email");
+                    String skype = contacts.getString("skype");
+
+                    Log.d(LOG_TAG, "phone: " + phone);
+                    Log.d(LOG_TAG, "email: " + email);
+                    Log.d(LOG_TAG, "skype: " + skype);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mobilization_music_app/MusiciansListActivity.java
+++ app/src/main/java/com/example/mobilization_music_app/MusiciansListActivity.java
@@ -9,6 +9,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MusiciansListActivity extends AppCompatActivity {
 
     @Override
@@ -18,6 +28,53 @@ public class MusiciansListActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.musiciansList);
 
+        // JSON part
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        // Copypasted from Internet
+        // Receiving data
+        @Override
+        protected String doInBackground {
+            try {
+                URL url = new URL("http://download.cdn.yandex.net/mobilization-2016/artists.json");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        ArrayList<String> musicians = new ArrayList<String>;
+
+        // String fake_json_string = getResources().getString(R.string.fake_json);
+        String fake_json_string = resultJson;
+        JSONArray jsonArray = new JSONArray(fake_json_string);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject musicianJson = jsonArray.getJSONObject(i);
+            String name = musicianJson.optString("name").toString();
+            musicians.add(name);
+        }
+
+
+
         // Temp array of musicians names
         final String[] musicians_names = new String[]{
                 "Prince", "Marilyn Manson", "Нюша",
@@ -25,7 +82,7 @@ public class MusiciansListActivity extends AppCompatActivity {
                 "Korn", "Katy Perry", "Usher"
         };
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, musicians_names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, musicians);
 
         listView.setAdapter(adapter);
 
